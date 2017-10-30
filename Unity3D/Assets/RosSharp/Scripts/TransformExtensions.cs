3b88be544b0380d49b36071f5e0d2001df3b3756
Martin Bischoff <martin.bischoff@siemens.com>
2017-10-30 15:18:33 +0100

    Unity3D reference project updates
    removed Examples and Tutorials folder (#14)
    added UrdfPatcher scripts and common RosBridgeClient Unity Scripts in  "/Unity3D" project
    updated RosSharp.unitypackage
    added experimental version of HugeJointLimits.cs

---
+++ Unity3D/Assets/RosSharp/Scripts/TransformExtensions.cs
@@ -0,0 +1,55 @@
+﻿/*
+© Siemens AG, 2017
+Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+
+namespace RosSharp
+{
+    public static class TransformExtensions
+    {
+        public static void SetParentAndAlign(this Transform transform, Transform parent, bool keepLocalTransform = true)
+        {
+            Vector3 localPosition = transform.localPosition;
+            Quaternion localRotation = transform.localRotation;
+            transform.parent = parent;
+            if (keepLocalTransform)
+            {
+                transform.position = transform.parent.position + localPosition;
+                transform.rotation = transform.parent.rotation * localRotation;
+            }
+            else
+            {
+                transform.localPosition = Vector3.zero;
+                transform.localRotation = Quaternion.identity;
+            }
+        }
+
+        public static Transform FindDeepChild(this Transform parent, string name)
+        {
+            Transform result = parent.Find(name);
+            if (result != null)
+                return result;
+            foreach (Transform child in parent)
+            {
+                result = child.FindDeepChild(name);
+                if (result != null)
+                    return result;
+            }
+            return null;
+        }
+    }
+}
