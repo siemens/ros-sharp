69bb76cd5ad2a7b7b8d98e2a7b2e2fa439ebc896
Jeremy Fix <jeremy.fix@gmail.com>
2017-12-07 12:02:50 +0100

    refactoring and taking into account all the angular velocities

---
+++ Unity3D/Assets/RosSharp/Scripts/VelocityTransformManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class VelocityTransformManager : MonoBehaviour
+    {
+	private Vector3 linearVelocity;
+	private Vector3 angularVelocity;
+
+	void Start()
+	{
+	    linearVelocity = Vector3.zero;
+	    angularVelocity = Vector3.zero;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.Translate (linearVelocity * Time.deltaTime);
+	    transform.Rotate(Vector3.forward, angularVelocity.x * Time.deltaTime);	
+	    transform.Rotate(Vector3.up     , angularVelocity.y * Time.deltaTime);	
+	    transform.Rotate(Vector3.left   , angularVelocity.z * Time.deltaTime);	
+	}
+
+	public void updateTransform(Vector3 _linearVelocity, Vector3 _angularVelocity)
+	{
+	    linearVelocity = _linearVelocity;
+	    angularVelocity = _angularVelocity;
+	}
+    }
+}
