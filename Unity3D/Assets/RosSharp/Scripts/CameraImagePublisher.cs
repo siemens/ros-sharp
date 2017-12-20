dead81f2fb79cb3a8ff9e1a6c69d89f7acd3a0d9
Martin Bischoff <martin.bischoff@siemens.com>
2017-12-20 13:30:59 +0100

    removing unneccessary libraries

--- Unity3D/Assets/RosSharp/Scripts/CameraImagePublisher.cs
+++ Unity3D/Assets/RosSharp/Scripts/CameraImagePublisher.cs
@@ -15,45 +15,45 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RosSharp.RosBridgeClient
 {
     [RequireComponent(typeof(RosConnector))]
-    public class CameraImagePublisher : MonoBehaviour {
+    public class CameraImagePublisher : MonoBehaviour
+    {
 
-	public string topic = "/image_raw/compressed";
+        public string topic = "/image_raw/compressed";
 
-	[Range(0, 100)]
-	public int qualityLevel = 50;
-	
-	private RosSocket rosSocket;
-	private int publicationId;
-	private SensorCompressedImage message;
-	private int sequenceId;
-	public string frameId = "camera";
+        [Range(0, 100)]
+        public int qualityLevel = 50;
 
-
-	void Start () {
-	    // The ROS part
-	    rosSocket = transform.GetComponent<RosConnector>().RosSocket;
-	    publicationId = rosSocket.Advertize (topic, "sensor_msgs/CompressedImage");
-	    message = new SensorCompressedImage ();
-	    sequenceId = 0;
-	}
+        private RosSocket rosSocket;
+        private int publicationId;
+        private SensorCompressedImage message;
+        private int sequenceId;
+        public string frameId = "camera";
 
 
-	public void Publish (Texture2D texture2D) {
-	    // Build up the message and publish
-	    message.header.frame_id = frameId;
-	    message.header.seq = sequenceId;
-	    message.format = "jpeg";
-	    message.data = texture2D.EncodeToJPG (qualityLevel);
-	    rosSocket.Publish (publicationId, message);
+        void Start()
+        {
+            // The ROS part
+            rosSocket = transform.GetComponent<RosConnector>().RosSocket;
+            publicationId = rosSocket.Advertize(topic, "sensor_msgs/CompressedImage");
+            message = new SensorCompressedImage();
+            sequenceId = 0;
+        }
 
-	    ++sequenceId;
-	}
+        public void Publish(Texture2D texture2D)
+        {
+            // Build up the message and publish
+            message.header.frame_id = frameId;
+            message.header.seq = sequenceId;
+            message.format = "jpeg";
+            message.data = texture2D.EncodeToJPG(qualityLevel);
+            rosSocket.Publish(publicationId, message);
+
+            ++sequenceId;
+        }
     }
 }
