5eee18fb83214f2d42a39b2b6d6c9179ea2dd76b
Martin Bischoff <martin.bischoff@siemens.com>
2018-05-17 11:40:09 +0200

    PointCloud.cs: Formatting & including new Message library

--- RosSharpLibraries/RosBridgeClient/PointCloud.cs
+++ RosSharpLibraries/RosBridgeClient/PointCloud.cs
@@ -16,6 +16,7 @@ limitations under the License.
 */
 
 using System;
+using RosSharp.RosBridgeClient.Messages;
 
 namespace RosSharp.RosBridgeClient
 {
@@ -34,7 +35,6 @@ namespace RosSharp.RosBridgeClient
                 Points[i] = new Point(byteSlice, sensorPointCloud2.fields);
             }
         }
-
 
         public PointCloud(SensorImage depthImage, SensorImage rgbImage, float focal)
         {
@@ -69,58 +69,58 @@ namespace RosSharp.RosBridgeClient
         }
     }
     public class Point
+    {
+        public float x;
+        public float y;
+        public float z;
+        public int[] rgb;
+
+        public Point(byte[] bytes, SensorPointField[] fields)
         {
-            public float x;
-            public float y;
-            public float z;
-            public int[] rgb;
-
-            public Point(byte[] bytes, SensorPointField[] fields)
+            foreach (var field in fields)
             {
-                foreach(var field in fields)
+                byte[] slice = new byte[field.count * 4];
+                Array.Copy(bytes, field.offset, slice, 0, field.count * 4);
+
+                switch (field.name)
                 {
-                    byte[] slice = new byte[field.count * 4];
-                    Array.Copy(bytes, field.offset, slice, 0, field.count * 4);
-                    
-                    switch (field.name)
-                    {
-                        case "x":
-                            x = getValue(slice);
-                            break;
-                        case "y":
-                            y = getValue(slice);
-                            break;
-                        case "z":
-                            z = getValue(slice);
-                            break;
-                        case "rgb":
-                            rgb = getRGB(slice);
-                            break;
-                    }
+                    case "x":
+                        x = getValue(slice);
+                        break;
+                    case "y":
+                        y = getValue(slice);
+                        break;
+                    case "z":
+                        z = getValue(slice);
+                        break;
+                    case "rgb":
+                        rgb = getRGB(slice);
+                        break;
                 }
-            }
-
-            public override string ToString()
-            {
-                return "xyz=(" + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ")"
-                    + "  rgb=(" + rgb[0].ToString() + ", " + rgb[1].ToString() + ", " + rgb[2].ToString() + ")";
-            }
-            private static float getValue(byte[] bytes)
-            {
-                if (!BitConverter.IsLittleEndian)
-                    Array.Reverse(bytes);
-
-                float result = BitConverter.ToSingle(bytes, 0);
-                return result;
-            }
-            private static int[] getRGB(byte[] bytes)
-            {
-                int[] rgb = new int[3];
-                rgb[0] = Convert.ToInt16(bytes[0]);
-                rgb[1] = Convert.ToInt16(bytes[1]);
-                rgb[2] = Convert.ToInt16(bytes[2]);
-                return rgb;
             }
         }
 
+        public override string ToString()
+        {
+            return "xyz=(" + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ")"
+                + "  rgb=(" + rgb[0].ToString() + ", " + rgb[1].ToString() + ", " + rgb[2].ToString() + ")";
+        }
+        private static float getValue(byte[] bytes)
+        {
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+
+            float result = BitConverter.ToSingle(bytes, 0);
+            return result;
+        }
+        private static int[] getRGB(byte[] bytes)
+        {
+            int[] rgb = new int[3];
+            rgb[0] = Convert.ToInt16(bytes[0]);
+            rgb[1] = Convert.ToInt16(bytes[1]);
+            rgb[2] = Convert.ToInt16(bytes[2]);
+            return rgb;
+        }
     }
+
+}
