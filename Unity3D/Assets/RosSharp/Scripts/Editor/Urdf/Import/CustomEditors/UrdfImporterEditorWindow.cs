f4bf1928b0362f73c5f177bd3b81daef09a6d515
Suzannah Smith <suzannah.smith@siemens.com>
2018-08-09 12:17:15 +0200

    Drastically changed RosSharp unity folder structure

---
+++ Unity3D/Assets/RosSharp/Scripts/Editor/Urdf/Import/CustomEditors/UrdfImporterEditorWindow.cs
@@ -0,0 +1,155 @@
+﻿/*
+© Siemens AG, 2017
+Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.IO;
+using System.Threading;
+using UnityEngine;
+using UnityEditor;
+
+namespace RosSharp.Urdf.Import
+{
+    public class UrdfImporterEditorWindow : EditorWindow
+    {
+        private readonly string[] Protocols = new string[] { "WebSocketSharpProtocol", "WebSocketNetProtocol" };
+        private static int protocolNumber;
+        private static string address;
+        private static int timeout;
+        private static string assetPath;
+      
+        private RosImportHandler importHandler;
+
+        [MenuItem("RosBridgeClient/Import URDF Assets...")]
+        private static void Init()
+        {
+            UrdfImporterEditorWindow editorWindow = GetWindow<UrdfImporterEditorWindow>();
+            editorWindow.minSize = new Vector2(150, 300);
+            
+            editorWindow.importHandler = new RosImportHandler();
+
+            editorWindow.Show();
+        }
+
+        private void OnFocus()
+        {
+            GetEditorPrefs();
+        }
+
+        private void OnLostFocus()
+        {
+            SetEditorPrefs();
+        }
+
+        private void OnDestroy()
+        {
+            SetEditorPrefs();
+        }
+
+        private void DeleteEditorPrefs()
+        {
+            EditorPrefs.DeleteKey("UrdfImporterProtocolNumber");
+            EditorPrefs.DeleteKey("UrdfImporterAddress");
+            EditorPrefs.DeleteKey("UrdfImporterAssetPath");
+            EditorPrefs.DeleteKey("UrdfImporterTimeout");
+        }
+        private void GetEditorPrefs()
+        {
+            protocolNumber = (EditorPrefs.HasKey("UrdfImporterProtocolNumber") ?
+                EditorPrefs.GetInt("UrdfImporterProtocolNumber") : 1);
+
+            address = (EditorPrefs.HasKey("UrdfImporterAddress") ?
+                EditorPrefs.GetString("UrdfImporterAddress") :
+                "ws://192.168.0.1:9090");
+
+            assetPath = (EditorPrefs.HasKey("UrdfImporterAssetPath") ?
+                EditorPrefs.GetString("UrdfImporterAssetPath") :
+                Path.Combine(Path.Combine(Path.GetFullPath("."), "Assets"), "Urdf"));
+
+            timeout = (EditorPrefs.HasKey("UrdfImporterTimeout") ?
+                EditorPrefs.GetInt("UrdfImporterTimeout") :
+                10);
+        }
+        private void SetEditorPrefs()
+        {
+            EditorPrefs.SetInt("UrdfImporterProtocol", protocolNumber);
+            EditorPrefs.SetString("UrdfImporterAddress", address);
+            EditorPrefs.SetString("UrdfImporterAssetPath", assetPath);
+            EditorPrefs.SetInt("UrdfImporterTimeout", timeout);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("URDF Asset Importer", EditorStyles.boldLabel);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUIUtility.labelWidth = 100;
+            protocolNumber = EditorGUILayout.Popup("Protocol", protocolNumber, Protocols);
+            address = EditorGUILayout.TextField("Address", address);
+            timeout = EditorGUILayout.IntField("Timeout [s]", timeout);
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            assetPath = EditorGUILayout.TextField("Asset Path", assetPath);
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Reset to Default", GUILayout.Width(150)))
+            {
+                DeleteEditorPrefs();
+                GetEditorPrefs();
+            }
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Space(20);
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Read Robot Description."))
+            {
+                SetEditorPrefs();
+
+                Thread rosSocketConnectThread = new Thread(() => importHandler.BeginRosImport(protocolNumber, address, timeout, assetPath));
+                rosSocketConnectThread.Start();
+            }
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Space(20);
+
+            EditorGUIUtility.labelWidth = 300;
+
+            DrawLabelField("1. rosbridge_server Connected:", "connected");
+            DrawLabelField("2. Robot Name Received:", "robotNameReceived");
+            DrawLabelField("3. Robot Description Received:", "robotDescriptionReceived");
+            DrawLabelField("4. Resource Files Received:", "resourceFilesReceived");
+            DrawLabelField("5. rosbridge_server Disconnected:", "disconnected");
+            DrawLabelField("6. Import Complete:", "importComplete");
+        }
+
+        private void DrawLabelField(string label, string stage)
+        {
+            GUIStyle guiStyle = new GUIStyle(EditorStyles.textField);
+            bool state = importHandler.StatusEvents[stage].WaitOne(0);
+            guiStyle.normal.textColor = state ? Color.green : Color.red;
+            EditorGUILayout.LabelField(label, state ? "done" : "open", guiStyle);
+        }
+
+        private void OnInspectorUpdate()
+        {
+            Repaint();
+
+            // some methods can only be called from main thread:
+            // We check the status to call the methods at the right step in the process:
+            importHandler.GenerateModelIfReady();
+        } 
+    }
+}
---
+++ Unity3D/Assets/RosSharp/Scripts/Editor/Urdf/Import/UrdfExtensions/UrdfLinkGeometryMeshExtensions.cs
@@ -0,0 +1,73 @@
+﻿/*
+© Siemens AG, 2017
+Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+using UnityEditor;
+
+namespace RosSharp.Urdf.Import
+{
+    public static class UrdfLinkGeometryMeshExtensions
+    {
+        public static GameObject CreateVisual(this Link.Geometry.Mesh mesh, GameObject parent)
+        {
+            GameObject meshObject = LocateAssetHandler.FindUrdfAsset<GameObject>(mesh.filename);
+
+            if(meshObject != null)
+            { 
+                GameObject gameObject = Object.Instantiate(meshObject);
+                mesh.setScale(gameObject);
+                gameObject.transform.SetParentAndAlign(parent.transform);
+                return gameObject;
+            }
+            return null;
+        }
+
+        public static GameObject CreateCollider(this Link.Geometry.Mesh mesh, GameObject parent)
+        {
+            GameObject prefabObject = LocateAssetHandler.FindUrdfAsset<GameObject>(mesh.filename);
+
+            if (prefabObject == null)
+                return null;
+                    
+            GameObject meshObject = Object.Instantiate(prefabObject);
+
+            MeshFilter[] meshFilters = meshObject.GetComponentsInChildren<MeshFilter>();
+            foreach (MeshFilter meshFilter in meshFilters)
+            {
+                GameObject child = meshFilter.gameObject;
+                MeshCollider meshCollider = child.AddComponent<MeshCollider>();
+                meshCollider.sharedMesh = meshFilter.sharedMesh;
+                Object.DestroyImmediate(child.GetComponent<MeshRenderer>());
+                Object.DestroyImmediate(meshFilter);
+            }
+
+            mesh.setScale(meshObject);
+            meshObject.transform.SetParentAndAlign(parent.transform);
+            return meshObject;
+        }
+
+        private static void setScale(this Link.Geometry.Mesh mesh, GameObject gameObject)
+        {
+            if (mesh.scale != null)
+            {
+                Vector3 scale = new Vector3((float)mesh.scale[0], (float)mesh.scale[1], (float)mesh.scale[2]);
+                gameObject.transform.localScale = Vector3.Scale(gameObject.transform.localScale, scale);
+                gameObject.transform.localPosition = Vector3.Scale(gameObject.transform.localPosition, scale);
+            }
+        }
+    }
+}
