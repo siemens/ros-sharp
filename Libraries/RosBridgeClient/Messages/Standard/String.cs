bf49868347632bca3ff6871ed1237dad3e717d1b
Berkay Alp Cakal <berkayalpc@gmail.com>
2019-02-08 16:07:23 +0100

    Revert "SimpleMessageGenerator and ActionMessageGenerator are added"
    This reverts commit 40c5c80101b1e21f6bde1f0d1e5fff3387452a94.

--- Libraries/RosBridgeClient/Messages/Standard/String.cs
+++ Libraries/RosBridgeClient/Messages/Standard/String.cs
@@ -22,9 +22,9 @@ namespace RosSharp.RosBridgeClient.Messages.Standard
         [JsonIgnore]
         public const string RosMessageName = "std_msgs/String";
         public string data;
-        public String()
+        public String(string data)
         {
-            data = "";
+            this.data = data;
         }
     }
 }
