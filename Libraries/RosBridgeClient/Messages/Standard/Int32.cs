11e7938d2e1e420ec85dea462aa56edc6bc5aebf
Lou Amadio <lamadio@microsoft.com>
2019-02-13 18:32:14 -0800

    Add Int message

---
+++ Libraries/RosBridgeClient/Messages/Standard/Int32.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Standard
+{
+    public class RosInt32 : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "std_msgs/Int32";
+        public Int32 data;
+        public RosInt32(Int32 data)
+        {
+            this.data = data;
+        }
+    }
+}
