3ab02f433d5e0140197043377fd6fbfa604db1d7
Abhay <abhayraw1@github.com>
2020-07-01 00:37:41 +0530

    Add check for freezeRotation for individual axis. Rigidbody.constraints enumeration defines each constraint as follows:  - None = 0  - FreezePositionX = 2  - FreezePositionY = 4  - FreezePositionZ = 8  - FreezePosition = 14  - FreezeRotationX = 16  - FreezeRotationY = 32  - FreezeRotationZ = 64  - FreezeRotation = 112  - FreezeAll = 126 Constraints are combined by adding individual ones. To check for absence of any rotational constraint, it should be <= 14.

--- Unity3D/Assets/RosSharp/Scripts/Urdf/Editor/CustomEditors/RigidbodyEditor.cs
+++ Unity3D/Assets/RosSharp/Scripts/Urdf/Editor/CustomEditors/RigidbodyEditor.cs
@@ -29,11 +29,11 @@ namespace RosSharp.Urdf.Editor
 
             Rigidbody _rigidbody = (Rigidbody)target;
             _rigidbody.centerOfMass = EditorGUILayout.Vector3Field("Center Of Mass", _rigidbody.centerOfMass);
-            if (!_rigidbody.freezeRotation)
+            if (_rigidbody.constraints <= RigidbodyConstraints.FreezePosition)
             {
                 _rigidbody.inertiaTensor = EditorGUILayout.Vector3Field("Inertia Tensor", _rigidbody.inertiaTensor);
             }
-            
+
             Quaternion inertiaTensorRotation = new Quaternion();
             inertiaTensorRotation.eulerAngles = EditorGUILayout.Vector3Field("Inertia Tensor Rotation", _rigidbody.inertiaTensorRotation.eulerAngles);
             _rigidbody.inertiaTensorRotation = inertiaTensorRotation;
