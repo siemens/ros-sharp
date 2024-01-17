f6961c52ab029ace877c320995b6975c7d477bd4
Mehmet Emre Cakal <emre.cakal@siemens.com>
2024-01-17 18:13:26 +0100

    feat(Entire Solution): Update to .NET 8.0 + WebSocketSharp-netstandard

--- Libraries/RosBridgeClient/Properties/AssemblyInfo.cs
+++ Libraries/RosBridgeClient/Properties/AssemblyInfo.cs
@@ -17,16 +17,6 @@ limitations under the License.
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("RosBridgeClient")]
-[assembly: AssemblyDescription(".NET API to ROS via rosbridge_suite")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Siemens AG")]
-[assembly: AssemblyProduct("ROS#")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -40,16 +30,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("27cd898a-8840-4cdd-a475-4a672fd2ef50")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
