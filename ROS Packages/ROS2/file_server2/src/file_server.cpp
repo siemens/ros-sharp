78a3d7dbb88df2b0b38063439c37434ed1b47149
Mehmet Emre Cakal <m.emrecakal@gmail.com>
2024-08-08 18:45:21 +0200

    feat(UPM Samples): Add old ROS1 UnitySimulationScene sample to the Unity package.

--- ROS Packages/ROS2/file_server2/src/file_server.cpp
+++ ROS Packages/ROS2/file_server2/src/file_server.cpp
@@ -1,3 +1,22 @@
+/*
+© Siemens AG, 2017-2018
+Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+- Added ROS2 support.
+- Added mutex and lock guards to make the save_file_callback thread-safe.
+    (C) Siemens AG, 2024, Mehmet Emre Cakal (emre.cakal@siemens.com/m.emrecakal@gmail.com)
+*/
+
 #include <rclcpp/rclcpp.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include "file_server2/srv/get_binary_file.hpp"
